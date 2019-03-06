7aff77c9f906f732058e91c4c688e9fc91980f66
Kaliszewski Michal (CI/DAD4.5) <Michal.Kaliszewski@pl.bosch.com>
2019-03-06 21:34:35 +0100

    init commit

---
+++ src/components/infinite-scroll/infinite-scroll.tsx
@@ -0,0 +1,18 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+    tag: 'infinite-scroll',
+})
+export class InfiniteScroll {
+
+    
+
+    render() {
+        return (
+            <div>
+                <p>Hello InfiniteScroll!</p>
+            </div>
+        );
+    }
+}
